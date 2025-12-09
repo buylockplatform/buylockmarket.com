1ec16982db83b8454873dfcfded29faaa7557dae
devjosiah11 <josiahkamau180@gmail.com>
2025-12-09 13:26:22 +0300

    run on local

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
@@ -73,12 +74,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
+
+
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+server.listen(port, () => {
+  log(`Server running on http://localhost:${port}`);
+});
 })();
